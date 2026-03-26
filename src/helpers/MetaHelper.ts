1e37d551b213c2b2f3c7b5603caa299ca5d58185
Trix <iamjimmymugo@gmail.com>
2026-03-26 16:03:35 +0300

    Replace all external ChurchApps links with Life Reformation Centre URLs

--- src/helpers/MetaHelper.ts
+++ src/helpers/MetaHelper.ts
@@ -21,7 +21,7 @@ export class MetaHelper {
   static getMetaData = (title?: string, description?: string, ogImage?: string, ogDescription?: string, appearance?: AppearanceInterface): Metadata => {
     if (!title) title = "Life Reformation Centre - Church Portal";
     if (!description) description = "Providing a simple and seemless way for your congregation to connect is a critical need in the modern church. Life Reformation Centre provides a way to do this at home, at church or wherever they may be, via their phone.";
-    if (!ogImage) ogImage = "https://content.churchapps.org/40/settings/ogImage.png?dt=1717512299395";
+    if (!ogImage) ogImage = "https://lifereformationcentre.org/images/og-image.png";
     if (!ogDescription) ogDescription = description;
 
     const metadata: Metadata = {
